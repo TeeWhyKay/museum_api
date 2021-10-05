961908214e17aef4094caebb283a4081257e8f4e
YongKeeTan <yongkee.t@gmail.com>
2021-10-06 01:13:00 +0800

    use key from .env

--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -2,7 +2,6 @@ require 'open-uri'
 
 class MuseumsController < ApplicationController
 
-  @mapbox_url = 'api.mapbox.com'
   def nearby
     lat = params[:lat].to_f
     lng = params[:lng].to_f
@@ -17,7 +16,8 @@ class MuseumsController < ApplicationController
   # output (.json) museum
   def search_museums(lat,lng)
     key = ENV['MAPBOX_API_KEY']
-    url = "https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?access_token=pk.eyJ1IjoidGVld2h5a2F5IiwiYSI6ImNrc3lkZXB5azJpbWoybmt0MXQwMGVib3UifQ.1elDNvnLbuHZ1dm8NOu8fg&proximity=#{lat}%2C#{lng}&limit=10"
+    byebug
+    url = "https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?access_token=#{key}&proximity=#{lat}%2C#{lng}&limit=10"
     result = {}
     mapbox_result_arr = JSON.parse(URI.open(url).read)["features"]
     mapbox_result_arr.each do |museum|
