50729469c2a429b9f3d5f5ac118dd3e6bd2a9e6c
YongKeeTan <yongkee.t@gmail.com>
2021-10-06 01:04:24 +0800

    routes update for json request

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/museums', to: 'museums#nearby'
+  # Rails would match /museums to the nearby action of museums controller, and set params[:format] to "json".
+  get '/museums', to: 'museums#nearby', defaults: { format: 'json' }
 end
