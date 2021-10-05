0c01419784484432a0fc383706a992e78da0fa03
YongKeeTan <yongkee.t@gmail.com>
2021-10-06 01:14:13 +0800

    formatting and removing byebug

--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -16,12 +16,11 @@ class MuseumsController < ApplicationController
   # output (.json) museum
   def search_museums(lat,lng)
     key = ENV['MAPBOX_API_KEY']
-    byebug
     url = "https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?access_token=#{key}&proximity=#{lat}%2C#{lng}&limit=10"
     result = {}
-    mapbox_result_arr = JSON.parse(URI.open(url).read)["features"]
+    mapbox_result_arr = JSON.parse(URI.open(url).read)['features']
     mapbox_result_arr.each do |museum|
-      result[museum["context"].first["text"]] = museum["text"]
+      result[museum['context'].first['text']] = museum['text']
     end
     result
   end
