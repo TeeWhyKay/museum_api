4c3d5ff9c0d843939d268c07cdc8b3d583a9eeaf
YongKeeTan <yongkee.t@gmail.com>
2021-10-07 16:47:51 +0800

    updated routes for API versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # Rails would match /museums to the nearby action of museums controller, and set params[:format] to "json".
-  get '/museums', to: 'museums#nearby', defaults: { format: 'json' }
+  # API versioning
+  namespace :api do
+    namespace :v1 do
+      get '/museums', to: 'museums#nearby', defaults: { format: 'json' }
+    end
+  end
 end
